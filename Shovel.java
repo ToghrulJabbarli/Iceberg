08ba563e5a37a42603e573fe40de5ad5bebf3565
ToghrulJabbarli <56371622+ToghrulJabbarli@users.noreply.github.com>
2020-03-29 00:03:49 +0100

    Updated by Ahsraf

--- Shovel.java
+++ Shovel.java
@@ -2,22 +2,24 @@ package Icefield;
 
 public class Shovel extends Object{
 
-@Override
-public void Used() {
+	@Override
+	public void Used() {
 	System.out.println("Shovel was used!");
-	
-}
+		used = true;
+	}
 
-@Override
-public boolean isUsed() {
+	@Override
+	public boolean isUsed() {
 	// Logic: Checks whether shovel is used or not
-	return false;
-}
+		return used;
+	}
 
-public void Use() {
+	public void Use() {
 	
-	// Logic
-}
+		if(this.used)
+			owner.removeSnow();
+		//there should be a printing in the function body in figure to state what happened
+	}
 
 
 }
