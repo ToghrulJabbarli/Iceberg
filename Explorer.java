9c6820978544e9ee6df48070f60c9ad2c3ac8d2d
achrefmekni <61314172+achrefmekni@users.noreply.github.com>
2020-03-28 20:36:15 +0100

    Update Explorer.java

--- Explorer.java
+++ Explorer.java
@@ -6,16 +6,20 @@ public class Explorer extends Figure{
 		super(Name, Health, SkillsCount, ItemsCount);
 	}
 	
-	@Override
+	
 	public void showOnGUI()
 	{
 		// Logic: show this object in GUI
 	}
 
-	@Override
-	public void Skill_Use()
+	//@Override // i added a parameter because the function needs to receive an iceberg :
+	
+	public void Skill_Use(Iceberg IC1)
 	{
 		// Logic: Check capacity of neighboring iceberg
+		System.out.printf("The Iceberg Capacity = ",IC1.capacity);
+		
 	}
 	
 }
+
