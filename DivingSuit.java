d60f3d91eec6fc66c2eb90accf0e91e5039cd172
ToghrulJabbarli <56371622+ToghrulJabbarli@users.noreply.github.com>
2020-03-29 00:01:18 +0100

    Updated by Ashraf

--- DivingSuit.java
+++ DivingSuit.java
@@ -10,17 +10,22 @@ public class DivingSuit extends Object{
 	@Override
 	public void Used() {
 		System.out.println("DivingSuit was taken!");
+		used = true;
 	}
 	@Override
 	public boolean isUsed() {
 		// Logic: Checks whether diving suit is used or not
-		return false;
+		return used;
 	}
 	
 public void Use() {
-		
+	
+	//THis will return the Figure to position (0,0), the origin point of the matrix
+	//It's a default position for all retrived figures.
+		if(used)
+			owner.Move(0, 0);
+		System.out.println("You have been rescued and moved to the default rescuing position");
 		// Logic
 	}
-
 	
 }
