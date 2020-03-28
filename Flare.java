e87afb3458625c5f53f237b5fe7178c832f2240a
ToghrulJabbarli <56371622+ToghrulJabbarli@users.noreply.github.com>
2020-03-28 23:58:25 +0100

    Update Flare.java

--- Flare.java
+++ Flare.java
@@ -4,12 +4,14 @@ public class Flare extends Object {
 	
 	@Override
 	public void Used() {
+		used = true;
 		System.out.println("Flare was taken!");
 	}
 	@Override
 	public boolean isUsed() {
 		// Logic: Checks whether flare is used or not
-		return false;
+		
+		return used;
 	}
 	
 	
