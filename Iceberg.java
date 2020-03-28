5f1536932b05fa23e9ffcc4e614ae8cd116301d2
ToghrulJabbarli <56371622+ToghrulJabbarli@users.noreply.github.com>
2020-03-28 23:49:42 +0100

    Updated by Murad & Toghrul

--- Iceberg.java
+++ Iceberg.java
@@ -6,6 +6,12 @@ import java.util.List;
 public class Iceberg extends Matrix{
 	
 	// Coordinates of the iceberg on the map
+	public static Integer IcebergID = 0;
+	public Integer IcebergName = 0;
+	
+	public Map map; // map of the game
+  
+	
 	private int x;
 	private int y;
 	//added by : Achref in order to use it in the Explorer class 
@@ -13,22 +19,54 @@ public class Iceberg extends Matrix{
 	// Blizzard on the iceberg
 	private Blizzard blizzard;
 
-	public Iceberg(int height, int width, int cap) {
+	public Iceberg(int height, int width, int cap, Map map) { // map is added
 		super(height, width);
 		capacity=cap;
+		this.map = map; // map of the game
 		// Properties of Blizzard is equal to the properties of the iceberg (so location and area)
-		blizzard = new Blizzard(height, width, x, y);
+		blizzard = new Blizzard(this);
+		IcebergName = IcebergID;
+		System.out.print("Iceberg " + IcebergID + " has been set up\n");
+		IcebergID++;
+		
 	}
 
-	public boolean isFittable()
+	public Integer getX()
 	{
-		// Logic: Check whether iceberg(Submatrix) is fittable into the Map(Matrix)
+		return x;
+	}
+	
+	public Integer getY()
+	{
+		return y;
+	}
+	
+	public boolean isFittable() // map parameter is added
+	{
+		// Logic: Check whether iceberg(Submatrix) is fittable into the Map(Matrix)  
 		// E.g Matrix 50x50, Submatrix 60 x 50 => Not fittable , Submatrix 23x22 => Fittable
-		return true;
+		if(map.width >= this.width && map.height >= this.height) // Furhter logic can be applied
+		{
+			System.out.print("Iceberg " + IcebergName + " is fit into the map.\n");
+			return true;
+		}
+		return false;
+	}
+	
+	public String getName()
+	{
+		return "Iceberg " + IcebergName;
 	}
 	
 	public void Fit(Map map)
 	{
 		// Logic: Fit the iceberg into the map if it is fittable
+		// BIN PACKING ALGORITHM CAN BE APPLIED
+		if(isFittable())
+		{
+			// Bin packing algorithms starts fitting the iceberg (submatrix)
+			System.out.print("Iceberg " + IcebergName + ": Bin packing algorithm starts...\n");
+		}
+		
 	}
 }
