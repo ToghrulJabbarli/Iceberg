344357ffa59cf5e3dba547e6e34f32aa1e0465f2
ToghrulJabbarli <56371622+ToghrulJabbarli@users.noreply.github.com>
2020-03-28 23:48:10 +0100

    Updated by Murad & Toghrul

--- Blizzard.java
+++ Blizzard.java
@@ -1,24 +1,25 @@
 package Icefield;
 
 public class Blizzard extends Matrix{
+	
+	Iceberg iceberg;
 
 	// Coordinates of the Blizzard
-	private int x;
-	private int y;
-
-	public Blizzard(int height, int width, int x, int y) {
-		super(height, width);
-		// TODO Auto-generated constructor stub
+	public Blizzard(Iceberg iceberg) {
+		super(iceberg.height, iceberg.width);
+		this.iceberg = iceberg;
 	}
 	
 	public void StartBlizzard()
 	{
 		// Logic: Start the Blizzard, Increment number of snow on the iceberg
+		System.out.print("Blizzard stars on (" + iceberg.getX() + " , " + iceberg.getY() + ")" );
 	}
 	
 	public void StopBlizzard()
 	{
 		// Logic: Stop the Blizzard
+		System.out.print("Bilard stops!");
 	}
 	
 
