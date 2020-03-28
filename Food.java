b08934021182c6a60b6a9d17f5e127abd9971bae
ToghrulJabbarli <56371622+ToghrulJabbarli@users.noreply.github.com>
2020-03-28 23:59:31 +0100

    Updated by Ashraf

--- Food.java
+++ Food.java
@@ -5,16 +5,20 @@ public class Food extends Object {
 	@Override
 	public void Used() {
 		System.out.println("Food was taken!");
+		used = true;
 	}
 	@Override
 	public boolean isUsed() {
 		// Logic: Checks whether food is used or not
-		return false;
+		return used;
 	}
 	
 public void Use() {
 		
 		// Logic
+	if(used)
+		owner.EatFood();
+	System.out.println("Food is used and your health is now"+ owner.get_health());
+
 	}
-	
 }
