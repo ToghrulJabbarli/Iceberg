35cb0f4e77d407b72a43bb52c7b41a7a14ed5642
ToghrulJabbarli <56371622+ToghrulJabbarli@users.noreply.github.com>
2020-03-29 00:02:22 +0100

    Updated by Ashraf

--- Charge.java
+++ Charge.java
@@ -10,13 +10,13 @@ public class Charge extends Object {
 	@Override
 	public void Used() {
 		System.out.print("Charge is used!");
-		
+		used = true;
 	}
 
 	@Override
 	public boolean isUsed() {
 		// Logic: Check whether charge is used or not!
-		return false;
+		return used;
 	}
 
 }
