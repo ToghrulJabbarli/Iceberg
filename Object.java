67e86d6075b5d0ae975f7ed537c2207726fd6a3d
ToghrulJabbarli <56371622+ToghrulJabbarli@users.noreply.github.com>
2020-03-28 23:57:20 +0100

    Updated by Ashraf

--- Object.java
+++ Object.java
@@ -3,9 +3,12 @@ package Icefield;
 public abstract class Object {
 	
 	public String Name;
-	
+	//++Each object get an owner after being retrived by this owner
+	public Figure owner;
 	// Condition for visibility of the object
 	public boolean isSeen;
+	//++
+	public boolean used = false;
 	
 	// Position of the object
 	public int x;
@@ -13,11 +16,7 @@ public abstract class Object {
 	
 	public abstract void Used(); // Object is consumed signal
 	public abstract boolean isUsed();
-	public void showOnGUI() {
-		// TODO Auto-generated method stub
 		
-	}
-	
 
 
 }
