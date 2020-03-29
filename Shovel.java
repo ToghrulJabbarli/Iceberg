1b326ee0bcaa0af89bed4c60eb11185d6da1fcff
achrefmekni <61314172+achrefmekni@users.noreply.github.com>
2020-03-30 01:39:06 +0200

    Update Shovel.java

--- Shovel.java
+++ Shovel.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class Shovel extends Object{
 
-	//all instances of this object will be the same name
-
 	public Shovel() {
 		this.Name = "Shovel";
 	}
 	
-	public void Use() {
+	//Achref: i ADDED  2 parameters since i changed the remove snow function
+	public void Use(Snow sn,int redlvl) {
 	
 		System.out.println("Do you want to use it? Press any key to confirm");
 		
 		Scanner sc = new Scanner(System.in);
 		char ch = sc.next().charAt(0);
 		
-		owner.removeSnow();
+		owner.removeSnow(sn,redlvl);
 		//there should be a printing in the function body in figure to state what happened
 	}
 
 
 }
+
