fe41b972ea011857221476b0d570979a9ab94748
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-03-29 15:16:23 +0200

    Update DivingSuit.java

--- DivingSuit.java
+++ DivingSuit.java
@@ -1,30 +1,27 @@
 package Icefield;
 
+import java.util.Scanner;
+
 public class DivingSuit extends Object{
 	
-	public void showOnGUI()
-	{
-		// Logic: show this object in GUI
-	}
-
-	@Override
-	public void Used() {
-		System.out.println("DivingSuit was taken!");
-		used = true;
-	}
-	@Override
-	public boolean isUsed() {
-		// Logic: Checks whether diving suit is used or not
-		return used;
+	public DivingSuit() {
+		this.Name = "Diving Suit";
 	}
 	
-public void Use() {
+	
+	public void Use() 
+	{
 	
 	//THis will return the Figure to position (0,0), the origin point of the matrix
 	//It's a default position for all retrived figures.
-		if(used)
-			owner.Move(0, 0);
+		System.out.println("Do you want to use it? Press any key to confirm");
+		
+		Scanner sc = new Scanner(System.in);
+		char ch = sc.next().charAt(0);
+			
 		System.out.println("You have been rescued and moved to the default rescuing position");
+		owner.Move(0, 0);
+		
 		// Logic
 	}
 	
