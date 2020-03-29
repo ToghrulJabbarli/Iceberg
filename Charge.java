8cf03c04fe370e0913050c95f64a13c3b1645f05
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-03-29 15:15:14 +0200

    Update Charge.java

--- Charge.java
+++ Charge.java
@@ -1,22 +1,11 @@
 package Icefield;
 
-public class Charge extends Object {
+import java.util.Scanner;
 
-	public void showOnGUI()
-	{
-		// Logic: show this object in GUI
-	}
-	
-	@Override
-	public void Used() {
-		System.out.print("Charge is used!");
-		used = true;
-	}
+public class Charge extends Object{
 
-	@Override
-	public boolean isUsed() {
-		// Logic: Check whether charge is used or not!
-		return used;
-	}
 
+	public Charge() {
+		this.Name = "Charge";
+	}
 }
