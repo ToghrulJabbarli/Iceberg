35c94edaec13b1712c41189d29e9cfde4227ab64
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-03-29 15:18:29 +0200

    Update GameEngine.java

--- GameEngine.java
+++ GameEngine.java
@@ -14,11 +14,12 @@ public class GameEngine {
 		// Logic: Get the winner
 		return map.getPlayers().get(0);
 	}
-	
-	public void setWinner()
+	//Ashraf: I added the attribute player which is the winner
+	//Ashraf: I made it static. There two ways here, later with the whole game integrated and this part is instantiated we can remove static
+	public static void setWinner(Player player)
 	{
 		// Logic: Setting up the winner based on logical operations
-		System.out.println(" The winner is : ");
+		System.out.println(" The winner is : " + player.getName());
 	}
 	
 	public void CellCondition()
