37b8b293db9d54aa4eafa5188f0304a30aa354d2
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-03-29 15:21:11 +0200

    Update StartProgram.java

--- StartProgram.java
+++ StartProgram.java
@@ -15,6 +15,8 @@ public class StartProgram {
 		 * Once figure is chosen, click "Done" button then another player can join and repeat above steps
 		 * If all players are set, tester can click "Start" button then MainMenu and Setting windows disappear and game starts
 		 */
+		
+		/*
 		GUI gui=new GUI(GUI.HEIGHT, GUI.WIDTH);
 		System.out.println("Welcome to Iceberg!");
 		System.out.println("Press 'A' to add Player");
@@ -65,7 +67,45 @@ public class StartProgram {
 		blizzard.StartBlizzard();
 		blizzard.StopBlizzard();
 		System.out.println("\n");
+		*/
+	
+	//Comment the previous part before starting here as this part is different separate scenarios that would be integrated later to make the sense of game.
+	//	-------------------------------------------------------------------------------------------------------------------------------
+	// During Testing, please test each object separately since each object represents a single separate scenario that only one can happen per MOVE
+		Figure figure1 = new Figure("Ashraf", 5, 2,4);
+		Figure figure2 = new Figure("Murad", 4,2 ,5);
 		
+		/*
+		Charge charge1 = new Charge();
+		charge1.Used(charge1, figure1);
+		
+		Flare f1 = new Flare();
+		f1.Used(f1, figure1);
+		
+		DivingSuit DS1 = new DivingSuit();
+		DS1.Used(DS1, figure1);
+		DS1.Use();
+		
+		Food food1 = new Food();
+		food1.Used(food1, figure1);
+		food1.Use();
+		
+		
+		Igloo igloo1 = new Igloo();
+		Igloo.Used(igloo1, figure1);
+		igloo1.Use();
+		
+		Rope rope1 = new Rope();
+		rope1.Used(rope1, figure1);
+		rope1.Use(figure2);
+		
+		Shovel shovel1 = new Shovel();
+		shovel1.Used(shovel1, figure1);
+		shovel1.Use();
+		
+		Snow snow1 = new Snow(3,4 ,3);
+		snow1.BuildingSnow();*/
+	
 		
 	}
 
