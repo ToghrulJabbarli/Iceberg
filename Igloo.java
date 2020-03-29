f1262fe093eed549ca7cf20f46f187be50aa7c4a
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-03-29 15:18:51 +0200

    Update Igloo.java

--- Igloo.java
+++ Igloo.java
@@ -1,29 +1,28 @@
 package Icefield;
 
+import java.util.Scanner;
+
 public class Igloo extends Object{
 
-	@Override
-	public void Used() {
-		System.out.println("Igloo was set!");
-		used = true;
+	public Igloo() {
+		this.Name = "Igloo";
 	}
-	@Override
-	public boolean isUsed() {
-		// Logic: Checks whether igloo is used or not
-		return used;
-	}
-	
+	//this function should be used in a way that it's called during the whole Blizzard period to keep health the same.
 public void Use() {
 		
 		// Logic
 	
-		if(this.used) {
+	System.out.println("Do you want to use it? Press any key to confirm");
+	
+	Scanner sc = new Scanner(System.in);
+	char ch = sc.next().charAt(0);
 			//keeps health the same during all the blizzard
 			//During all the blizzard, the figure can call this function to save his health
-			System.out.println("Igloo has been built. Owner of this Igloo shall not feel any pain during this blizzard.")
-			owner.set_health(owner.get_health());
-			owner.Skill_Use();
-		}
+			System.out.println("Igloo has been built. Owner of this Igloo shall not feel any pain during this blizzard.");
+			owner.setHealth(owner.getHealth());
+			System.out.println("your health now is:" + owner.getHealth());
+			//We should call SkillUse() here but it needs further implementation especially in GUI.
+		
 	}
 
 
