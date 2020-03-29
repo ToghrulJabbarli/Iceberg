4b8739716f9af377d1799d27bb60f856df3a26a9
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-03-29 22:24:18 +0200

    Update Flare.java

--- Flare.java
+++ Flare.java
@@ -3,7 +3,9 @@ package Icefield;
 import java.util.Scanner;
 
 public class Flare extends Object {
-	
+
+	//all instances of this object will be the same name
+
 	public Flare() {
 		this.Name = "Flare";
 	}
