d12a152813b87546789e787f2a751060576b1812
ToghrulJabbarli <56371622+ToghrulJabbarli@users.noreply.github.com>
2020-03-30 13:18:51 +0200

    Updated

--- StartProgram.java
+++ StartProgram.java
@@ -20,6 +20,56 @@ public class StartProgram {
 		gui.ShowGUI();
 	}
 
+	public static void MuradandToghrulTest() {
+		System.out.println(" --------------------MURAD & TOGHRUL------------------- \n");
+		
+		
+		List<Player> players = new ArrayList<Player>();
+		Player player_1 = new Player("Murad");
+		Player player_2 = new Player("Toghrul");
+		players.add(player_1);
+		players.add(player_2);
+		
+		GUI gui=new GUI(GUI.HEIGHT, GUI.WIDTH);
+		
+		Map map = new Map(25,25,players);
+		map.showOnGUI(gui, map);
+		for(int i = 0; i < players.size(); i++)
+		{
+			System.out.println("Player " + i + ": ID: " + players.get(i).getID() + ", Name: " + players.get(i).getName());
+		}
+		
+		
+		
+		// Iceberg
+		Iceberg i_1 = new Iceberg(5,5,10,map);
+		System.out.println("Iceberg is fittable: " + i_1.isFittable());
+		System.out.println("Iceberg name: " + i_1.getName());
+		i_1.Fit(map);
+		
+		System.out.print("\n\n");
+		
+		Iceberg i_2 = new Iceberg(5,5,10,map);
+		System.out.println("Iceberg is fittable: " + i_2.isFittable());
+		System.out.println("Iceberg name: " + i_2.getName());
+		i_2.Fit(map);
+		
+		Iceberg[] icebergs = new Iceberg[2];
+		icebergs[0] = i_1;
+		icebergs[1] = i_2;
+		
+		
+		map.SetIcebergs(icebergs);
+		map.GenerateIcebergsOnMap();
+		
+		// Blizzard
+		Blizzard blizzard = new Blizzard(i_1);
+		blizzard.StartBlizzard();
+		blizzard.StopBlizzard();
+		
+	}
+	
+	
 	public static void main(String[] args) throws Throwable {
 		
 		// TODO Auto-generated method stub
@@ -34,7 +84,7 @@ public class StartProgram {
 			int choice=Integer.valueOf(result);
 			switch (choice) {
 			case 1:
-				
+				MuradandToghrulTest();
 				break;
 			case 2:
 				break;
