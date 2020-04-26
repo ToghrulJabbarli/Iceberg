c3cc0805b2c1c1a3eb68968b535b0931be7b891d
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-04-26 19:17:48 +0200

    Updating Input and Output

--- Shovel.java
+++ Shovel.java
@@ -1,5 +1,6 @@
 package Icefield;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Shovel extends Object{
@@ -9,16 +10,23 @@ public class Shovel extends Object{
 	}
 	
 	//Snow that would be removed
-	public void Use(Snow sn) {
+	public void Use(Snow sn, String input, String output) throws IOException {
 	
 		System.out.println("Do you want to use it? Press any key to confirm");
-		
+		WriteFile(output,"Do you want to use it? Press any key to confirm" );
+
+		/*
 		Scanner sc = new Scanner(System.in);
 		char ch = sc.next().charAt(0);
+		*/
+		
 	
+		if(ReadFile (input, "dig"))
+	    {
 		owner.removeSnow(sn);
+		this.used = true;
+	    }
 	}
 
 
 }
-
