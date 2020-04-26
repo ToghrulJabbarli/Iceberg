da6e1220e94cbf81d12bb69bb109b30a08018b65
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-04-26 19:16:45 +0200

    Updating Input and Output

--- Igloo.java
+++ Igloo.java
@@ -1,5 +1,6 @@
 package Icefield;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Igloo extends Object{
@@ -10,21 +11,22 @@ public class Igloo extends Object{
 		this.Name = "Igloo";
 	}
 	//this function should be used in a way that it's called during the whole Blizzard period to keep health the same.
-public void Use() {
+	public void Use(String output) throws IOException {
 		
 		// Logic
 	
-	System.out.println("Do you want to use it? Press any key to confirm");
-	
-	Scanner sc = new Scanner(System.in);
-	char ch = sc.next().charAt(0);
 			//keeps health the same during all the blizzard
 			//During all the blizzard, the figure can call this function to save his health
 			System.out.println("Igloo has been built. Owner of this Igloo shall not feel any pain during this blizzard.");
+			WriteFile(output,"Do you want to use it? Press any key to confirm" );
+
 			owner.setHealth(owner.getHealth());
 			System.out.println("your health now is:" + owner.getHealth());
+			WriteFile(output,"Do you want to use it? Press any key to confirm" );
+
 			//We should call SkillUse() here but it needs further implementation especially in GUI.
-		
+			this.used = true;
+
 	}
 
 
