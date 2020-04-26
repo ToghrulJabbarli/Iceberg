203a1428c79ebad4f8894a4aa9d4f37ea08e0c3d
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-04-26 19:17:17 +0200

    Updating Input and Output

--- Rope.java
+++ Rope.java
@@ -1,28 +1,33 @@
 package Icefield;
 
+import java.io.IOException;
 import java.util.Scanner;
 
-public class Rope extends Object{
+public class Rope extends Object
+{
 
 	//all instances of this object will be the same name
 
-	public Rope() {
+	public Rope() 
+	{
 		this.Name = "Rope";
 	}
 	
-	public void Use(Figure rescued_figure) {
+	public void Use(Figure rescued_figure, String input, String output) throws IOException 
+	{
 		
 		System.out.println("Do you want to use it? Press any key to confirm");
-		
-		Scanner sc = new Scanner(System.in);
-		char ch = sc.next().charAt(0);
-		
-		System.out.println(rescued_figure.getName() + "has been rescued successfully !");
-		rescued_figure.Move(0,0);
-		
+		WriteFile(output,"Do you want to use it? Press any key to confirm" );
+
+
+		if(ReadFile (input, "use on " + rescued_figure.getName() ))
+	    {
+			System.out.println(rescued_figure.getName() + "has been rescued successfully !");
+			this.WriteFile(output, rescued_figure.getName() + "has been rescued successfully !");
+
+			rescued_figure.Move(0,0);
+			this.used = true;
+
+	    }
 	}
-	
-
-	
-
 }
