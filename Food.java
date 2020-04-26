0f9c80766ecf6722ac646c162de39dc84f5c9eec
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-04-26 19:15:56 +0200

    Updating Input and Output

--- Food.java
+++ Food.java
@@ -1,27 +1,34 @@
 package Icefield;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Food extends Object {
-
-	//all instances of this object will be the same name
 
 	public Food() {
 		this.Name = "Food";
 	}
 	
-public void Use() {
+public void Use(String input, String output) throws IOException {
 		
 		// Logic
-
-	System.out.println("Do you want to use it? Press any key to confirm");
 	
+	System.out.println("Do you want to use it? Press any key to confirm");
+	WriteFile(output,"Do you want to use it? Press any key to confirm" );
+
+	/*
 	Scanner sc = new Scanner(System.in);
 	char ch = sc.next().charAt(0);
-	
-	System.out.println("Food is used and your health is now"+ (owner.getHealth()+1));
-	//This function is to be implemented with more details later based on the requirements
-	//owner.EatFood();
+	*/
+	if(ReadFile(input, "use"))
+		{
+		owner.EatFood();
+		System.out.println("Food is used, you health has increas and is now"+ (owner.getHealth()));
+		WriteFile(output,"Food is used, you health has increas and is now"+ (owner.getHealth()) );
+
+		this.used = true;
+
+		}
 
 	}
 }
