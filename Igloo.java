cc56a2c109518752756189cbc4a63be5f4571249
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-04-27 14:31:29 +0200

    Update Igloo.java

--- Igloo.java
+++ Igloo.java
@@ -11,18 +11,16 @@ public class Igloo extends Object{
 		this.Name = "Igloo";
 	}
 	//this function should be used in a way that it's called during the whole Blizzard period to keep health the same.
-	public void Use(String output) throws IOException {
+	public void Use() {
 		
 		// Logic
 	
 			//keeps health the same during all the blizzard
 			//During all the blizzard, the figure can call this function to save his health
 			System.out.println("Igloo has been built. Owner of this Igloo shall not feel any pain during this blizzard.");
-			WriteFile(output,"Do you want to use it? Press any key to confirm" );
 
 			owner.setHealth(owner.getHealth());
 			System.out.println("your health now is:" + owner.getHealth());
-			WriteFile(output,"Do you want to use it? Press any key to confirm" );
 
 			//We should call SkillUse() here but it needs further implementation especially in GUI.
 			this.used = true;
