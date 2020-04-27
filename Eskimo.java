a822c5ec68126e7950793cdfb5f8603340780158
achrefmekni <61314172+achrefmekni@users.noreply.github.com>
2020-04-27 13:35:11 +0200

    Update Eskimo.java

--- Eskimo.java
+++ Eskimo.java
@@ -6,12 +6,12 @@ package Icefield;
 
 public class Eskimo extends Figure{
 	
-	public Eskimo(String Name, int Health, int SkillsCount, int ItemsCount) {
-		super(Name, Health, SkillsCount, ItemsCount);
+	public Eskimo(String Name, int Health, int SkillsCount, int ItemsCount,Player player) {
+		super(Name, Health, SkillsCount, ItemsCount,player);
 	}
 
 	@Override
-	public void Skill_Use()
+	public void Skill_Use() throws IOException
 	{
 		// Logic: build igloo
 		Igloo ig= new Igloo();
@@ -20,9 +20,10 @@ public class Eskimo extends Figure{
 		
 		//logic to build the igloo ( position on the map )
 		// depends on mouse position 
-		
+		ig.Use();
 		System.out.println("Igloo is built ");
 	}
 	
 
 }
+
