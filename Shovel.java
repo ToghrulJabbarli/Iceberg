a5d73a8c1656b64e39f40278835be7bae2e659bc
achrefmekni <61314172+achrefmekni@users.noreply.github.com>
2020-04-27 13:30:06 +0200

    Update Shovel.java

--- Shovel.java
+++ Shovel.java
@@ -1,6 +1,4 @@
 package Icefield;
-
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Shovel extends Object{
@@ -10,23 +8,17 @@ public class Shovel extends Object{
 	}
 	
 	//Snow that would be removed
-	public void Use(Snow sn, String input, String output) throws IOException {
+	//Achref : i edited use since i also edited remove snow 
+	public void Use(int x , int y) {
 	
 		System.out.println("Do you want to use it? Press any key to confirm");
-		WriteFile(output,"Do you want to use it? Press any key to confirm" );
-
-		/*
+		
 		Scanner sc = new Scanner(System.in);
 		char ch = sc.next().charAt(0);
-		*/
-		
 	
-		if(ReadFile (input, "dig"))
-	    {
-		owner.removeSnow(sn);
-		this.used = true;
-	    }
+		owner.removeSnow(x,y);
 	}
 
 
 }
+
