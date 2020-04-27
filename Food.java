7a85f236a5b0edd35d708a6919b9ba8a0f0a01a1
Dominat10 <56002796+Dominat10@users.noreply.github.com>
2020-04-27 14:30:58 +0200

    Update Food.java

--- Food.java
+++ Food.java
@@ -9,26 +9,24 @@ public class Food extends Object {
 		this.Name = "Food";
 	}
 	
-public void Use(String input, String output) throws IOException {
+public void Use() {
 		
 		// Logic
 	
 	System.out.println("Do you want to use it? Press any key to confirm");
-	WriteFile(output,"Do you want to use it? Press any key to confirm" );
 
-	/*
+	
 	Scanner sc = new Scanner(System.in);
 	char ch = sc.next().charAt(0);
-	*/
-	if(ReadFile(input, "use"))
-		{
+	
+	
+		
 		owner.EatFood();
 		System.out.println("Food is used, you health has increas and is now"+ (owner.getHealth()));
-		WriteFile(output,"Food is used, you health has increas and is now"+ (owner.getHealth()) );
 
 		this.used = true;
 
 		}
 
 	}
-}
+
